88f71461d2a980eb8c54d3ee7c54bac1b698b23b
Undefined Name <74594673+udfn@users.noreply.github.com>
2021-05-26 13:45:26 +0300

    Make nwl be a submodule

--- subprojects/nwl/nwl/cairo.h
+++
@@ -1,9 +0,0 @@
-#ifndef _NWL_CAIRO_H
-#define _NWL_CAIRO_H
-#include "nwl/surface.h"
-#include <cairo.h>
-
-typedef bool (*nwl_surface_cairo_render_t)(struct nwl_surface *surface, cairo_surface_t *cairo_surface);
-void nwl_surface_renderer_cairo(struct nwl_surface *surface, nwl_surface_cairo_render_t renderfunc);
-
-#endif
